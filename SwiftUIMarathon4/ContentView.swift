658a5435ec31e7c38b67749a732a809974235bf8
enneract9 <vargunin.iv@gmail.com>
2024-12-10 22:59:16 +0500

    Refactoring

--- SwiftUIMarathon4/ContentView.swift
+++ SwiftUIMarathon4/ContentView.swift
@@ -8,76 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var performAnimation = false
-
     var body: some View {
-        Button {
-            if !performAnimation {
-                withAnimation(
-                    Animation.spring(duration: 0.4, bounce: 0.3)
-                ) {
-                    performAnimation = true
-                } completion: {
-                    performAnimation = false
-                }
-            }
-        } label: {
-            GeometryReader { proxy in
-                let width = proxy.size.width / 2
-                let systemName = "play.fill"
-                
-                HStack(alignment: .center, spacing: 0) {
-                    Image(systemName: systemName)
-                        .renderingMode (.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: performAnimation ? width : .zero)
-                        .opacity(performAnimation ? 1 : .zero)
-                    Image(systemName: systemName)
-                        .renderingMode (.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: width)
-                    Image(systemName: systemName)
-                        .renderingMode (.template)
-                        .resizable ()
-                        .scaledToFit ()
-                        .frame(width: performAnimation ? 0.5 : width)
-                        .opacity(performAnimation ? .zero : 1)
-                }
-                .frame(maxHeight: .infinity, alignment: .center)
-            }
-        }
-        .frame(width: 60)
-        .buttonStyle(PinokStyle())
+        
+        Button("Play") {}
+            .buttonStyle(.playNext)
+            .frame(width: 60)
     }
 }
 
-struct PinokStyle: ButtonStyle {
+extension ButtonStyle where Self == PlayNextButtonStyle {
+    @MainActor @preconcurrency internal static var playNext: PlayNextButtonStyle { PlayNextButtonStyle() }
+}
+
+struct PlayNextButtonStyle: ButtonStyle {
     
-    @State private var performAnimation = false
+    @State private var performLabelAnimation = false
+    @State private var performBackgroundAnimation = false
 
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label
+        label()
             .background {
                 background()
             }
-            .scaleEffect(performAnimation ? 0.86 : 1.0)
+            .scaleEffect(performBackgroundAnimation ? 0.86 : 1.0)
             .onChange(of: configuration.isPressed) {
                 withAnimation(
                     .easeOut(duration: 0.22)
                 ) {
-                    performAnimation.toggle()
+                    performBackgroundAnimation.toggle()
+                }
+                
+                if !performLabelAnimation && configuration.isPressed {
+                    withAnimation(
+                        Animation.spring(duration: 0.4, bounce: 0.3)
+                    ) {
+                        performLabelAnimation = true
+                    } completion: {
+                        performLabelAnimation = false
+                    }
                 }
             }
+    }
+    
+    private func label() -> some View {
+        GeometryReader { proxy in
+            let width = proxy.size.width / 2
+            
+            HStack(alignment: .center, spacing: 0) {
+                playImage()
+                    .frame(width: performLabelAnimation ? width : .zero)
+                    .opacity(performLabelAnimation ? 1 : .zero)
+                playImage()
+                    .frame(width: width)
+                playImage()
+                    .frame(width: performLabelAnimation ? 0.5 : width)
+                    .opacity(performLabelAnimation ? .zero : 1)
+            }
+            .frame(maxHeight: .infinity, alignment: .center)
+        }
+    }
+    
+    private func playImage() -> some View {
+        Image(systemName: "play.fill")
+            .renderingMode (.template)
+            .resizable()
+            .scaledToFit()
     }
     
     private func background() -> some View {
         Circle()
             .padding(-16)
-            .foregroundColor(
-                .gray.opacity(performAnimation ? 0.16 : .zero)
-            )
+            .foregroundColor(.gray.opacity(performBackgroundAnimation ? 0.16 : .zero))
     }
 }
 
